3101892c844e785909d17941d0eace5e406ce255
Doskulov Mirbek <iskender.doskulov@gmail.com>
2023-03-04 21:25:28 +0600

    Admin + DashBoard add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const App = () => {
-	return <div>App</div>
+	return (
+		<div className='h-header bg-main w-full'>
+			<h1 className='text-h1 text-star font-bold underline'></h1>
+			<p className='text-white'></p>
+		</div>
+	)
 }
 
 export default App
