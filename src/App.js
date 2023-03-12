701556316716f862a2896792e763cdd2b504517f
Doskulov Mirbek <iskender.doskulov@gmail.com>
2023-03-12 16:22:32 +0600

    Login + Register

--- src/App.js
+++ src/App.js
@@ -16,8 +16,10 @@ import { Route, Routes } from 'react-router-dom'
 import AboutUs from './Screens/AboutUs'
 import ContactUs from './Screens/ContactUs'
 import HomeScreen from './Screens/HomeScreen'
+import Login from './Screens/Login'
 import MoviesPage from './Screens/Movies'
 import NotFound from './Screens/NotFound'
+import Register from './Screens/Register'
 import SingleMovie from './Screens/SingleMovie'
 import WatchPage from './Screens/WatchPage'
 
@@ -31,6 +33,8 @@ function App() {
 			<Route path='/movies' element={<MoviesPage />} />
 			<Route path='/movie/:id' element={<SingleMovie />} />
 			<Route path='/watch/:id' element={<WatchPage />} />
+			<Route path='/login' element={<Login />} />
+			<Route path='/register' element={<Register />} />
 			<Route path='*' element={<NotFound />} />
 		</Routes>
 	)
---
+++ src/Screens/Register.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Input } from '../Components/UsedInputs'
+import Layout from '../layout/Layout'
+import { FiLogIn } from 'react-icons/fi'
+
+function Register() {
+	return (
+		<Layout>
+			<div className='container mx-auto px-2 my-24 flex-colo'>
+				<div className='w-full 2xl:w-2/5 gap-8 flex-colo p-8 sm:p-14 md:w-3/5 bg-dry  rounded-lg border border-border'>
+					<img
+						src='https://etnomedia.kg/assets/images/logo.svg'
+						alt='logo'
+						className='w-full h-12 object-contain'
+					/>
+					<Input
+						label='FullName'
+						placeholder='Netflixo React Tailwind'
+						type='text'
+						bg={true}
+					/>
+					<Input
+						label='Email'
+						placeholder='netflixo@gmail.com'
+						type='email'
+						bg={true}
+					/>
+					<Input
+						label='Password'
+						placeholder='*******'
+						type='password'
+						bg={true}
+					/>
+					<Link
+						to='/dashboard'
+						className='bg-subMain transitions hover:bg-main flex-rows gap-4 text-white p-4 rounded-lg w-full'
+					>
+						<FiLogIn /> Sign Up
+					</Link>
+					<p className='text-center text-border'>
+						Already have an account?{' '}
+						<Link to='/login' className='text-dryGray font-semibold ml-2'>
+							Sign In
+						</Link>
+					</p>
+				</div>
+			</div>
+		</Layout>
+	)
+}
+
+export default Register
